6e0d77e5f01abde93d6958f1ca996672755370b3
Chiznera <chiznera@gmail.com>
2022-07-07 01:14:06 +0000

    work on python models

--- src/api/routes.py
+++ src/api/routes.py
@@ -16,3 +16,72 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+
+@api.route('/people', methods=['GET'])
+def get_people():
+    person = People.query.all().one_or_none()
+    if person is None:
+        return jsonify(msg="This page does not exist."), 400
+    else:
+        return jsonify(
+        data=[person.serialize() for person in people]), 200
+
+@api.route('/people/<int:id>', methods=['GET'])
+def get_people(id):
+    person = People.query.filter(
+        People.id == id
+    ).one_or_none()
+    if person is None:
+        return jsonify(msg="This person does not exist."), 400
+    else:
+        return jsonify(
+        data=person.serialize()
+        ), 200
+
+
+
+@api.route('/planets', methods=['GET'])
+def get_planets():
+    planet = Planets.query.all().one_or_none()
+    if planet is None:
+        return jsonify(msg="This page does not exist."), 400
+    else:
+        return jsonify(
+        data=[planet.serialize() for planet in planets]), 200
+
+@api.route('/planets/<int:id>', methods=['GET'])
+def get_planets(id):
+    planet = Planets.query.filter(
+        Planets.id == id
+    ).one_or_none()
+    if planet is None:
+        return jsonify(msg="This planet does not exist."), 400
+    else:
+        return jsonify(
+        data=person.serialize()
+        ), 200
+
+
+
+@api.route('/vehicles', methods=['GET'])
+def get_vehicles():
+    vehicle = Vehicles.query.all().one_or_none()
+    if vehicle is None:
+        return jsonify(msg="This page does not exist."), 400
+    else:
+        return jsonify(
+        data=[person.serialize() for person in people]), 200
+
+@api.route('/people/<int:id>', methods=['GET'])
+def get_people(id):
+    person = People.query.filter(
+        People.id == id
+    ).one_or_none()
+    if person is None:
+        return jsonify(msg="This person does not exist."), 400
+    else:
+        return jsonify(
+        data=person.serialize()
+        ), 200
